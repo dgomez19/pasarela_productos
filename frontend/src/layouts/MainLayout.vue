b4bff05475b6f36f94a3720124e6bfb3e9b81439
Daniel Gómez <daniel.gomez@itagui.gov.co>
2024-08-27 00:26:57 -0500

    Maqueta inicial del frontend en Vue.js

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header bordered class="bg-white text-dark">
+      <q-toolbar class="q-px-md q-py-xs bg-secondary text-white">
+        <strong>Wompi</strong>
+      </q-toolbar>
+
+      <q-toolbar class="flex-center q-pa-md" style="min-height: auto;">
+        <div class="row items-center q-gutter-x-md" style="width: 100%; max-width: 1024px">
+
+          <div class="column">
+            <div class="text-subtitle1 text-bold">Productos</div>
+            <div class="text-body2">&nbsp;</div>
+          </div>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <footer class="bg-primary text-white q-py-lg" style="margin-top: 80px">
+      <q-toolbar class="flex-center">
+        <q-card flat bordered class="row bg-white text-dark rounded-borders q-pa-xl" style="width: 100%; max-width: 1024px; margin-top: -80px">
+          <div class="col-12 col-sm-11 row">
+            <div class="column text-body2">
+              <div class="text-h6 text-primary">
+                <img src="~/assets/logo.svg" alt="colombia_logo" class="cursor-pointer" />
+              </div>
+
+            </div>
+          </div>
+          <div class="col-12 col-sm-1 flex justify-center">
+          </div>
+        </q-card>
+      </q-toolbar>
+    </footer>
+
+    <footer class="bg-secondary text-dark q-px-md q-py-sm">
+    </footer>
+
+  </q-layout>
+</template>
+
+<script setup>
+</script>
---
+++ frontend/src/pages/IndexPage.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-md-4 form-group">
+        <q-card class="my-card" style="margin:10px 13px 1px 1px;">
+          <img src="~/assets/logo.svg" alt="colombia_logo" class="cursor-pointer" />
+
+          <q-card-section>
+            <div class="text-h6">Producto prueba</div>
+            <div class="text-subtitle2">Comprar</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            {{ lorem }}
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <div class="col-md-4 form-group">
+        <q-card class="my-card" style="margin:10px 13px 1px 1px;">
+          <img src="~/assets/logo.svg" alt="colombia_logo" class="cursor-pointer" />
+
+          <q-card-section>
+            <div class="text-h6">Producto prueba</div>
+            <div class="text-subtitle2">Comprar</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            {{ lorem }}
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <div class="col-md-4 form-group">
+        <q-card class="my-card" style="margin:10px 13px 1px 1px;">
+          <img src="~/assets/logo.svg" alt="colombia_logo" class="cursor-pointer" />
+
+          <q-card-section>
+            <div class="text-h6">Producto prueba</div>
+            <div class="text-subtitle2">Comprar</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            {{ lorem }}
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-4 form-group">
+        <q-card class="my-card" style="margin:10px 13px 1px 1px;">
+          <img src="~/assets/logo.svg" alt="colombia_logo" class="cursor-pointer" />
+
+          <q-card-section>
+            <div class="text-h6">Producto prueba</div>
+            <div class="text-subtitle2">Comprar</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            {{ lorem }}
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <div class="col-md-4 form-group">
+        <q-card class="my-card" style="margin:10px 13px 1px 1px;">
+          <img src="~/assets/logo.svg" alt="colombia_logo" class="cursor-pointer" />
+
+          <q-card-section>
+            <div class="text-h6">Producto prueba</div>
+            <div class="text-subtitle2">Comprar</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            {{ lorem }}
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <div class="col-md-4 form-group">
+        <q-card class="my-card" style="margin:10px 13px 1px 1px;">
+          <img src="~/assets/logo.svg" alt="colombia_logo" class="cursor-pointer" />
+
+          <q-card-section>
+            <div class="text-h6">Producto prueba</div>
+            <div class="text-subtitle2">Comprar</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            {{ lorem }}
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </div>
+</template>
